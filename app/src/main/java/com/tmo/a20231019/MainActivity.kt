adf71b3ff8e3fadbac56e03767403d284d53d046
tomasmonteiro03 <76120194+tomasmonteiro03@users.noreply.github.com>
2023-10-19 20:52:22 +0100

    commit com screenshot e codigo

--- app/src/main/java/com/tmo/a20231019/MainActivity.kt
+++ app/src/main/java/com/tmo/a20231019/MainActivity.kt
@@ -3,13 +3,21 @@ package com.tmo.a20231019
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.tmo.a20231019.ui.theme._20231019Theme
 
 class MainActivity : ComponentActivity() {
@@ -22,13 +30,13 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                          //Greeting("Android")
                 }
             }
         }
     }
 }
-
+/*
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
@@ -44,3 +52,23 @@ fun GreetingPreview() {
         Greeting("Android")
     }
 }
+*/
+
+@Composable
+fun RowDate(){
+    Row{
+        Column {
+            Text(text = "Tomás Monteiro")
+            Text(text = "19/10/2023")
+        }
+        Image(painter = painterResource(id = R.drawable.cars__1_), contentDescription = "", modifier = Modifier.size(48.dp).clip(
+            CircleShape))
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun RowDatePreview(){
+    RowDate()
+}
+
